2c70d45d76abff394676b9f137591868f61f0dbf
Robert Cunningham <robertvcunningham@gmail.com>
2026-04-29 12:41:56 -0700

    fix: initialize analytics in app

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,7 +1,15 @@
 import '@/styles/globals.css'
 import 'react-tooltip/dist/react-tooltip.css'
+import LogRocket from 'logrocket'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      LogRocket.init('phqm4b/zeptovolt')
+    }
+  }, [])
+
   return <Component {...pageProps} />
 }
--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -1,6 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document"
-import LogRocket from "logrocket"
-import { useEffect } from "react"
 
 export default function Document() {
   return (
@@ -9,18 +7,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <Analytics />
       </body>
     </Html>
   )
-}
-
-const Analytics = () => {
-  useEffect(() => {
-    if (process.env.NODE_ENV === "production") {
-      LogRocket.init("phqm4b/zeptovolt")
-    }
-  }, [])
-
-  return <></>
 }
